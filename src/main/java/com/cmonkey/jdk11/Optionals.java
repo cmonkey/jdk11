4056d3d237133c32b86ba22bf2c86e366545ffd6
CMonkey <42.codemonkey@gmail.com>
2018-09-26 14:59:46 +0800

    add Optionals

--- src/main/java/com/cmonkey/jdk11/Optionals.java
+++ src/main/java/com/cmonkey/jdk11/Optionals.java
@@ -1,8 +1,18 @@
 package com.cmonkey.jdk11;
 
+import java.util.Optional;
+
 public class Optionals {
 
     public static void main(String[] args) {
-        
+        var foo = Optional.of("foo").orElseThrow();
+        var count = Optional.of("foo").stream().count();
+
+        System.out.println(foo);
+        System.out.println(count);
+
+        var op = Optional.ofNullable(null).or(() -> Optional.of("fallback")).get();
+
+        System.out.println(op);
     }
 }
