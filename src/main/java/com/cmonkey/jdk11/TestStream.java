37c180a0f238412750a61d174e6a0acd2cdc879b
CMonkey <42.codemonkey@gmail.com>
2018-09-26 14:53:37 +0800

    add Stream.dropWhile method to TestStream

--- src/main/java/com/cmonkey/jdk11/TestStream.java
+++ src/main/java/com/cmonkey/jdk11/TestStream.java
@@ -1,5 +1,6 @@
 package com.cmonkey.jdk11;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestStream {
@@ -8,5 +9,11 @@ public class TestStream {
         var count = Stream.ofNullable(null).count();
 
         System.out.println(count);
+
+        var list = Stream.of(1,2,3,2,1)
+                .dropWhile(n -> n < 3)
+                .collect(Collectors.toList());
+
+        list.forEach(item -> System.out.println(item));
     }
 }
